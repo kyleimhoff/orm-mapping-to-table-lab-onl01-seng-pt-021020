7aae50c2951e3299c7ae8330da4d8e871739e983
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-04-28 01:05:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,4 +22,24 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.drop_table 
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].exicute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
+    
+    SQL
+    DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
 end
